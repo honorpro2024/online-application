03591db4be9039f342e3910a5aa82f322cc4aa69
honorpro2024 <mknott@honorproviders.com>
2025-04-29 11:02:50 -0400

    Add files via upload

---
+++ app/Notifications/User/PasswordResetNotification.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Notifications\User;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Notifications\Notification;
+
+class PasswordResetNotification extends Notification implements ShouldQueue
+{
+    use Queueable;
+
+    public $user, $url;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct($user, $url)
+    {
+        $this->user = $user;
+        $this->url = $url;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('Forgot Password')
+            ->line("Hello " . $this->user->name . ", you can reset your password from below link. ")
+            ->action('Reset Password', $this->url)
+            ->line('This password reset link will expire in 60 minutes.')
+            ->line('If you did not request a password reset, no further action is required.');
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
