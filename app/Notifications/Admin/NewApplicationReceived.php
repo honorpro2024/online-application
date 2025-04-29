b938a639ccbb31a0f5a3a98571f2385532637f2a
honorpro2024 <mknott@honorproviders.com>
2025-04-29 11:02:00 -0400

    Add files via upload

---
+++ app/Notifications/Admin/NewApplicationReceived.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Notifications\Admin;
+
+use App\Models\User;
+use Illuminate\Bus\Queueable;
+use Illuminate\Notifications\Notification;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Notifications\Messages\MailMessage;
+
+class NewApplicationReceived extends Notification
+{
+    use Queueable;
+
+    public $user;
+
+    /**
+     * Create a new notification instance.
+     *
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Get the notification's delivery channels.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function via($notifiable)
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return \Illuminate\Notifications\Messages\MailMessage
+     */
+    public function toMail($notifiable)
+    {
+        return (new MailMessage)
+            ->subject('New Application Received')
+            ->line('You received new application. Click on the below button to view the application.')
+            ->action('View Application', route('admin.users.show', $this->user));
+    }
+
+    /**
+     * Get the array representation of the notification.
+     *
+     * @param  mixed  $notifiable
+     * @return array
+     */
+    public function toArray($notifiable)
+    {
+        return [
+            //
+        ];
+    }
+}
+
